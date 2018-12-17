ca559a5f0082c4920a8fb531f7da573b2da466ec
Luis Esteban Rodriguez <rodriguezjluis0@gmail.com>
2018-12-17 13:52:09 -0600

    display Quiz questions and handling events

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,6 +12,12 @@ class Quiz extends Component {
         }
     }
 
+    showNextQuestion() {
+    	this.setState((state) => {
+    		return {quiz_position: state.quiz_position + 1}
+    	})
+    }
+
     render() {
     	const isQuizEnd = ((this.state.quiz_position -1 ) === quizData.quiz_questions.length)
         return (
@@ -19,6 +25,7 @@ class Quiz extends Component {
             	{ isQuizEnd ? <QuizEnd /> : 
             	<QuizQuestion
             		quiz_question={ quizData.quiz_questions[this.state.quiz_position - 1] }
+            		showNextQuestionHandler={ this.showNextQuestion.bind(this) }
             	/>}
             </div>
         );
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -6,6 +6,12 @@ class QuizQuestion extends Component {
         super(props);
     }
 
+    handleClick(buttonText) {
+    	if (buttonText === this.props.quiz_question.answer) {
+    		this.props.showNextQuestionHandler();
+    	}
+    }
+
     render() {
         return (
         	<main>
@@ -16,7 +22,10 @@ class QuizQuestion extends Component {
         		</section>
         		<section className="buttons">
           			<ul>
-            			<QuizQuestionButton button_text={ this.props.quiz_question.answer_options[0] } />
+          				{ this.props.quiz_question.answer_options.map((answer_option, index) => {
+          					return <QuizQuestionButton key={ index } button_text={ answer_option } 
+          					clickHandler={ this.handleClick.bind(this) } />
+          				})}
           			</ul>
         		</section>
       		</main>
